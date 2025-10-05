7f4c6592c5e062317a3f2ff1ff26d7da3fdab0f9
ondrejnyklicek <ondrejnyklicek@icloud.com>
2025-10-05 16:03:40 +0200

    Init

---
+++ src/Providers/LivewireTableServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace NyonCode\LivewireTable\Providers;
+
+use NyonCode\LaravelPackageToolkit\Contracts\Packable;
+use NyonCode\LaravelPackageToolkit\Packager;
+use NyonCode\LaravelPackageToolkit\PackageServiceProvider;
+use Exception;
+
+
+class LivewireTableServiceProvider extends PackageServiceProvider implements Packable
+{
+    /**
+     * Configures the package. This method is called after the package is registered.
+     *
+     * @param Packager $packager
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function configure(Packager $packager): void
+    {
+        $packager->name('Laravel Livewire Table')
+            ->hasViews()
+            ->hasAbout();
+    }
+
+    public function aboutData(): array
+    {
+        return [
+            'Author' => 'Ondrej Nyklicek',
+            'Author Email' => 'ondrej@nyoncode.cz',
+            'Author URL' => 'https://nyoncode.cz',
+            'License' => 'MIT',
+            'License URL' => 'https://opensource.org/licenses/MIT',
+            'Description' => 'Livewire table',
+        ];
+    }
+}
